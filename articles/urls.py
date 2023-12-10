299d8a820cba47ae37bef77a79c8e41fc6f4592a
pit0500 <pietro.macri2000@gmail.com>
2023-12-10 11:37:59 +0100

    Added editors to urls

--- articles/urls.py
+++ articles/urls.py
@@ -1,9 +1,10 @@
 
 from django.urls import path
 from rest_framework.routers import SimpleRouter
-from articles.views import ArticleViewSet
+from articles.views import ArticleEditorViewSet, ArticleViewSet
 
 router = SimpleRouter()
 router.register('', ArticleViewSet, basename='articles')
+router.register('editor', ArticleEditorViewSet, basename='article-editors')
 
 urlpatterns = router.urls
