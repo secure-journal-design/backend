c3897e3dabefb3584ad650e93fb1eb46e21ab8a5
Fd3231 <doria.francesco27.fd@gmail.com>
2023-12-14 22:18:31 +0100

    updated picture

--- backend/urls.py
+++ backend/urls.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework.documentation import include_docs_urls
 from rest_framework.schemas import get_schema_view
+from django.conf import settings
+from django.conf.urls.static import static
 
 API_TITLE = "Secure Journal Design"
 API_DESCRIPTION = "API for Secure Journal"
@@ -15,3 +17,6 @@ urlpatterns = [
     path('api/v1/auth/', include('dj_rest_auth.urls')),
     path('api/v1/auth/registration/', include('dj_rest_auth.registration.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
